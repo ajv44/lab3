98e6e10f5e35e4a0b934c53ef01c668635c8bd1d
ajv44 <avillalino@ucsd.edu>
2023-10-24 17:17:44 -0700

    Added the reverseInPlace() and reversed()

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,22 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  static void reverseInPlace(int[] arr) {
+    int[] newArray = new int[arr.length];
+    for(int i = 0; i < arr.length/2; i += 1) {
+      int temp = arr[i];
+      arr[i] = arr[arr.length - i - 1] = temp;
+    }
+  }
+
+  @Test
+  static int[] reversed(int[] arr) {
+    int[] newArray = new int[arr.length];
+    for(int i = 0; i < arr.length; i += 1) {
+      newArray[i] = arr[arr.length - i - 1];
+    }
+    return newArray; 
+  }
 }
